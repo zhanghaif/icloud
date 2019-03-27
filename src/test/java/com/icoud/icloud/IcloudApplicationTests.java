6fdb7ab5164e74ffb549d4abfede4bbc26615e74
zhanghaifeng <123892897@qq.com>
2019-03-27 18:44:57 +0800

    打包时跳过单元测试代码

--- src/test/java/com/icoud/icloud/IcloudApplicationTests.java
+++ src/test/java/com/icoud/icloud/IcloudApplicationTests.java
@@ -1,20 +1,20 @@
-//package com.icoud.icloud;
-//
-//import static cn.com.icloud.core.common.ProjectConstant.MAPPER_PACKAGE;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import tk.mybatis.spring.annotation.MapperScan;
-//
-//@SpringBootApplication
-//@MapperScan(basePackages = MAPPER_PACKAGE)
-//@EnableAutoConfiguration(exclude = {
-//		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
-//public class IcloudApplicationTests {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(IcloudApplicationTests.class, args);
-//	}
-//
-//}
+package com.icoud.icloud;
+
+import static cn.com.icloud.core.common.ProjectConstant.MAPPER_PACKAGE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan(basePackages = MAPPER_PACKAGE)
+@EnableAutoConfiguration(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
+public class IcloudApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcloudApplicationTests.class, args);
+	}
+
+}
